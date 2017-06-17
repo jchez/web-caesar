ce806bcc7e774940cd9ca2e2a71cde4ea1412dea
Jonathan Sanchez <jonsanchezu@gmail.com>
2017-06-16 23:06:58 -0700

    imported caesar code and processed form

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request
+from caesar import rotate_string
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -24,12 +25,32 @@ form = """
         </style>
     </head>
     <body>
+        <form action="/change" method="post">
+            <label for="rot">
+                Rotate by: 
+                <input type="text" name="rot" value="0"/>
+            </label>
+            <textarea name="text">
+            </textarea>
+            <input type="submit" value ="Submit Query"/>
+        </form>
     </body>
 </html> 
 """
 
+@app.route("/change", methods=['POST'])
+def encrypt():
+    rotation = request.form['rot']
+    rotation2 = int(rotation)
+    characters = request.form['text']
+    final = rotate_string(characters, rotation2)
+    content = "<h1>" + final + "</h1>"
+    return content
+
+
 @app.route("/")
 def index():
-    return "Hello World"
+    content = form
+    return content
 
 app.run()
